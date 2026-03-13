bef11aa7ae31ab4d28f68d20e6fb2996b67d73b5
Hoang Phong <hoangphong210703@gmail.com>
2026-03-13 09:47:01 +0700

    Rfr: naming

--- dags/brz2stg_processing.py
+++ dags/brz2stg_processing.py
@@ -56,7 +56,7 @@ def sequence_loads(**kwargs):
 
 
 with DAG(
-    dag_id="processing",
+    dag_id="brz2stg_processing",
     description="Sequence table loads and trigger ingestion",
     schedule=None,
     start_date=datetime(2024, 1, 1),
